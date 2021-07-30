01fdfca1cabf4ca6023a9e180497fc59aab6fc78
Rounok Das <rounokdasrkd@gmail.com>
2021-07-31 05:00:53 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar ul').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+        // typing text animation script
+        var typed = new Typed(".typing", {
+            strings: ["Ml Expert", "Web Developer", "Core Java Developer", "App Designer", "Freelancer"],
+            typeSpeed: 100,
+            backSpeed: 60,
+            loop: true
+        });
+
+    // typing text animation script
+    var typed = new Typed(".typing-2", {
+        strings: ["Ml Expert", "Web Developer", "Core Java Developer", "App Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
+});
